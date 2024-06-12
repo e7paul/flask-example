1f0a26b63c41c2f69ec8387088a8efc1c7317627
e7paul <e7paul1@gmail.com>
2024-06-12 13:26:43 +0400

    edit, delete operations

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
 from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
@@ -9,6 +9,14 @@ def get_db_connection():
     conn = sqlite3.connect('main.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def get_entry(entry_id):
+    conn = get_db_connection()
+    entries = conn.execute('SELECT * FROM entries WHERE id = ?', (entry_id,)).fetchone()
+    conn.close()
+    if entries is None:
+        abort(404)
+    return entries
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
 db = SQLAlchemy(app)
@@ -29,11 +37,11 @@ def create():
         gender = request.form.get('gender', '')
 
         if not number:
-            flash('number is required!')
+            flash('number is required!', 'error')
         elif not datetime:
-            flash('datetime is required!')
+            flash('datetime is required!', 'error')
         elif not gender:
-            flash('gender is required!')
+            flash('gender is required!', 'error')
         else:
             conn = get_db_connection()
             conn.execute('INSERT INTO entries (number, datetime, isExit, gender) VALUES (?, ?, ?, ?)',
@@ -42,6 +50,42 @@ def create():
             conn.close()
             return redirect(url_for('index'))
     return render_template('create.html')
+
+@app.route('/<int:id>/edit/', methods=('GET', 'POST'))
+def edit(id):
+    entry = get_entry(id)
+
+    if request.method == 'POST':
+        number = request.form.get('number', '')
+        datetime = request.form.get('datetime', '')
+        isExit = int(request.form.get('isExit', 0))
+        gender = request.form.get('gender', '')
+
+        if not number:
+            flash('number is required!', 'error')
+        elif not datetime:
+            flash('datetime is required!', 'error')
+        elif not gender:
+            flash('gender is required!', 'error')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE entries SET number = ?, datetime = ?, isExit = ?, gender = ? WHERE id = ?',
+                (number, datetime, isExit, gender, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', entry=entry)
+
+@app.route('/<int:id>/delete/', methods=('GET',))
+def delete(id):
+    entry = get_entry(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM entries WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"Entry No. {}" was successfully deleted!'.format(entry['number']), 'info')
+    return redirect(url_for('index'))
 
 app.run(debug=True)
 
